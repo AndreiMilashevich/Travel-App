aa0c4820f8a4ce09b545d5cec6c4175da96d1dc5
AndreiMilashevich <72551064+AndreiMilashevich@users.noreply.github.com>
2023-03-23 01:02:03 +0300

    Revert "TrApp-001 Create Login page"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
+import { LoginComponent } from './login/login.component';
 
-
-const importedModules = [
-  MatButtonModule,
-  MatTabsModule,
-  MatInputModule,
-  MatIconModule,
-  MatCardModule,
-];
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent,
+    LoginComponent
   ],
   imports: [
-    ...importedModules,
     BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginPageComponent } from './login-page.component';
+import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
-  let component: LoginPageComponent;
-  let fixture: ComponentFixture<LoginPageComponent>;
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginPageComponent ]
+      declarations: [ LoginComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(LoginPageComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
