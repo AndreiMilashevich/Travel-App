edcdc24f6d313f0bddcf4b1081524606bd6c1f51
Andrei Milashevich <AndreiMilashevich@gmail.com>
2023-03-24 22:06:38 +0100

    TrApp-002-ESLint - update lint config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
 import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 
 const importedModules = [
-  MatButtonModule,
-  MatTabsModule,
-  MatInputModule,
-  MatIconModule,
-  MatCardModule,
+	MatButtonModule,
+	MatTabsModule,
+	MatInputModule,
+	MatIconModule,
+	MatCardModule,
 ];
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginPageComponent,
-  ],
-  imports: [
-    ...importedModules,
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LoginPageComponent,
+	],
+	imports: [
+		...importedModules,
+		BrowserModule,
+		BrowserAnimationsModule,
+		AppRoutingModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
 export class AppModule { }
